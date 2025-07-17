5d7011b74a0723a3ca4172793fae36d67f572db1
GNOMIK <dimadimych2008@gmail.com>
2025-07-17 19:49:35 +0200

    MVP 0.0.1.2

--- Uneven-Path/config.py
+++ Uneven-Path/config.py
@@ -1,2 +1,3 @@
-# Configuration file for the bot
-API_TOKEN = '7990662954:AAEKd4qtgUqJ98A8ugtuum7ut9q49cs_ako'  # Replace with your token from @BotFather
+import os
+
+API_TOKEN = os.getenv('API_TOKEN')
